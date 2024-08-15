d28f50effdfa7c5759060fc2ca219d3c511c2d71
ganesh gone <ganeshgone2003@gmail.com>
2024-08-16 00:58:04 +0530

    built navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkcV0e3Yimgqeaph_KaoOMtDJfmeigOU8",
+    authDomain: "challenge-dd4c8.firebaseapp.com",
+    projectId: "challenge-dd4c8",
+    storageBucket: "challenge-dd4c8.appspot.com",
+    messagingSenderId: "205822109671",
+    appId: "1:205822109671:web:489ad3dcbec2b87bef3f42",
+    measurementId: "G-2VC1CF69RF"
+  };
