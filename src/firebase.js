44be22d27159fcef38f408ced4ae92d7503de9b9
ganesh gone <ganeshgone2003@gmail.com>
2024-08-17 14:26:04 +0530

    built cart and authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBkcV0e3Yimgqeaph_KaoOMtDJfmeigOU8",
     authDomain: "challenge-dd4c8.firebaseapp.com",
@@ -7,4 +11,13 @@ const firebaseConfig = {
     messagingSenderId: "205822109671",
     appId: "1:205822109671:web:489ad3dcbec2b87bef3f42",
     measurementId: "G-2VC1CF69RF"
-  };
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Auth
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
